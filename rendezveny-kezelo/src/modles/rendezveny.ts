9cdeecd4c6edac65eee878685a2ef2a01b03f395
Hanna-ai591 <hanna.kariotis@gmail.com>
2025-06-11 17:07:28 +0200

    3

--- rendezveny-kezelo/src/modles/rendezveny.ts
+++ rendezveny-kezelo/src/modles/rendezveny.ts
@@ -1,7 +1,25 @@
 export interface IRendezvény {
     nev: string;
-    helyszín: string;
+    helyszin: string;
     ido: Date;
     tematika:string;
     resztvevok:string[];
+}
+export class Rendezveny implements IRendezvény{
+    nev: string;
+    helyszin: string;
+    ido: Date;
+    tematika: string;
+    resztvevok: string[];
+
+    constructor(nev: string, helyszin: string, ido: Date, tematika:string){
+    this.nev = nev;
+    this.helyszin = helyszin;
+    this.ido = ido;
+    this.tematika = tematika;
+    this.resztvevok = [];
+    }
+    addResztvevo(resztvevoNev: string){
+        this.resztvevok.push(resztvevoNev);
+    }
 }
