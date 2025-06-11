0782127689a04458e5730fd0dd9b5af948b219f1
Hanna-ai591 <hanna.kariotis@gmail.com>
2025-06-11 18:01:52 +0200

    13

--- rendezveny-kezelo/src/index.ts
+++ rendezveny-kezelo/src/index.ts
@@ -1,5 +1,5 @@
 import { Rendezveny } from "./models/rendezveny"
-import { RendezvenyService } from "./seervices/rendezvenyService"
+import { RendezvenyService } from "./services/rendezvenyService"
 
 const service = new RendezvenyService();
 const fesztival = new Rendezveny("Nyári fesztivál", "Budapest", new Date("2025-07-16"), "Zene");
