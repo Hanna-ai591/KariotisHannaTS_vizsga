1fc5339fc02d4f99f96cb059f6639d93fbb5fecb
Hanna-ai591 <hanna.kariotis@gmail.com>
2025-06-11 18:15:29 +0200

    14

--- rendezveny-kezelo/src/index.ts
+++ rendezveny-kezelo/src/index.ts
@@ -1,7 +1,15 @@
 import { Rendezveny } from "./models/rendezveny"
 import { RendezvenyService } from "./services/rendezvenyService"
+import { Resztvevo } from "./models/resztvevo";
 
 const service = new RendezvenyService();
 const fesztival = new Rendezveny("Nyári fesztivál", "Budapest", new Date("2025-07-16"), "Zene");
 service.addRendezvenyek(fesztival);
-console.log(service.getrendezvenyek());
+const resztvevo1 = new Resztvevo("John Doe", "john.doe@gmail.com");
+service.addResztvevoToRendezveny("NYári fesztivál", resztvevo1);
+
+console.log("Rendezvények:");;
+service.getrendezvenyek().forEach(r => {
+    console.log(` -${r.nev}, ${r.tematika}, helyszín: ${r.helyszin}, résztvevők száma: ${r.resztvevok.length}`);
+});
+ 
