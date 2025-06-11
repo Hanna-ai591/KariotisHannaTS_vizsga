79faaca59330883192e4166b1b7166524776463b
Hanna-ai591 <hanna.kariotis@gmail.com>
2025-06-11 18:42:08 +0200

    17

---
+++ rendezveny-kezelo/src/decorators/logAddition.ts
@@ -0,0 +1,12 @@
+export function LOgAddition(
+  target:any,
+  propertyKey:String,
+  descriptor: PropertyDescriptor
+){
+    const originalMethod = descriptor.value;
+    descriptor.value = function (...args:any[]){
+        console.log("Hozzáadás történt a listához");
+        return originalMethod.apply(this, args);
+        
+    }
+}
